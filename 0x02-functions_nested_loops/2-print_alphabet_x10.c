bf2f1ed3eab94a2344640ce9589375e138b9c15f
MohanadOraby <mohanad97@aucegypt.edu>
2023-07-26 07:41:00 -0700

    fuk betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
 
 void print_alphabet_x10(void)
 {
-        char letter;
+	char letter;
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-	        for(letter = 'a'; letter <= 'z'; letter ++)
-	        {
-        	        _putchar(letter);
-	        }
-        _putchar('\n');
+		for(letter = 'a'; letter <= 'z'; letter ++)
+		{
+		       	_putchar(letter);
+		}
+	_putchar('\n');
 	}
 }
