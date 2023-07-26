fe0f1f5b800db7dd1b9bc3c20d2144be791fe199
MohanadOraby <mohanad97@aucegypt.edu>
2023-07-26 09:42:08 -0700

    that was cancer

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,22 +8,40 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int result;
+
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-		
-			if (j == 9)
+			result = i * j;
+
+			if (result <= 9)
 			{
-				_putchar((i*j) + '0');
+				 if (j == 9)
+                                {
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar(result + '0');
+                                }
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((result + '0'));
+				_putchar(',');
 			}
 			else
 			{
-				_putchar((i*j) + '0');
-                        	_putchar(',');
-                        	_putchar(' ');
-                        	_putchar(' ');
+				if (j == 9)
+				{
+					_putchar(' ');
+					_putchar((result/10) + '0');
+					_putchar((result%10) + '0');
+				}
+				_putchar(' ');
+				_putchar((result/10) + '0');
+				_putchar((result%10) + '0');
+				_putchar(',');
 			}
 		}
 	_putchar('\n');
