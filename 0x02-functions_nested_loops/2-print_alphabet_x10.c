341d2e440154c3998fd576daab0bbba09a53a168
MohanadOraby <mohanad97@aucegypt.edu>
2023-07-26 07:50:39 -0700

    y

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for(letter = 'a'; letter <= 'z'; letter ++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
-		       	_putchar(letter);
+			_putchar(letter);
 		}
 	_putchar('\n');
 	}
