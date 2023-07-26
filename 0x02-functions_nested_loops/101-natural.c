564a02dffa5188d5b26ae531a05e2ebd30f0680c
MohanadOraby <mohanad97@aucegypt.edu>
2023-07-26 10:25:43 -0700

    ??

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,6 @@
 /*
  * File: 101-natural.c
- * Auth: vincent kip
+ * Made by: MoH
  */
 
 #include <stdio.h>
@@ -13,15 +13,14 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	int i; 
+	int sum;
+	sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
+			sum = sum + i;
 	}
-
-	printf("%d\n", sum);
-
 	return (0);
 }
